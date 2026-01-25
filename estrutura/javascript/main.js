782ef5af8f1bd10f433092ca205a8a3867882226
mateus <mateuspontesdsilva11@gmail.com>
2026-01-25 16:19:03 -0300

    javascript aplicado.
    Co-Authored-By: Vitória <222024999+vitorialinhares@users.noreply.github.com>

--- estrutura/javascript/main.js
+++ estrutura/javascript/main.js
@@ -3,3 +3,19 @@ window.addEventListener("scroll", () => {
     document.querySelector(".menu").classList.add("fixo");
   }
 });
+
+function filtrarProdutos() {
+  const input = document.getElementById("pesquisa");
+  const filtro = input.value.toLowerCase();
+  const produtos = document.querySelectorAll(".produto");
+
+  produtos.forEach(produto => {
+    const textoProduto = produto.innerText.toLowerCase();
+
+    if (textoProduto.includes(filtro)) {
+      produto.style.display = "flex";
+    } else {
+      produto.style.display = "none";
+    }
+  });
+}
